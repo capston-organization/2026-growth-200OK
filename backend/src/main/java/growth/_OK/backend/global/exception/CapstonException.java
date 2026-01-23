73ba069e6c1751c86bf8e20bcfc3db223839f766
9rin <flyrin1208@gmail.com>
2026-01-23 22:12:27 +0900

    feat: error handler

---
+++ backend/src/main/java/growth/_OK/backend/global/exception/CapstonException.java
@@ -0,0 +1,15 @@
+package growth._OK.backend.global.exception;
+
+import org.springframework.http.HttpStatusCode;
+
+public class CapstonException extends RuntimeException {
+    private final ExceptionCode exceptionCode;
+    public CapstonException(ExceptionCode exceptionCode) {
+        this.exceptionCode = exceptionCode;
+    }
+
+    @Override
+    public String getMessage() {return exceptionCode.getMessage();}
+    public HttpStatusCode getHttpStatusCode() {return exceptionCode.getHttpStatus();}
+    public String getExceptionCodeName() {return exceptionCode.getClientExceptionCode().name();}
+}
---
+++ backend/src/main/java/growth/_OK/backend/global/exception/ExceptionCode.java
@@ -0,0 +1,29 @@
+package growth._OK.backend.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ExceptionCode {
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ClientExceptionCode.INTERNAL_SERVER_ERROR, "예상치 못한 서버에러가 발생했습니다."),
+    ILLEGAL_ARGUMENT(HttpStatus.BAD_REQUEST, ClientExceptionCode.ILLEGAL_ARGUMENT, "필수 파라미터 누락"),
+
+    // 유저
+    AUTH_TOKEN_EMPTY(HttpStatus.UNAUTHORIZED, ClientExceptionCode.AUTH_TOKEN_EMPTY, "다시 로그인해주세요."),
+    ACCESS_TOKEN_EMPTY(HttpStatus.UNAUTHORIZED, ClientExceptionCode.ACCESS_TOKEN_EMPTY, "엑세스 토큰이 존재하지 않습니다. 다시 로그인해주세요."),
+    REFRESH_TOKEN_EMPTY(HttpStatus.UNAUTHORIZED, ClientExceptionCode.REFRESH_TOKEN_EMPTY, "리프레시 토큰이 존재하지 않습니다."),
+    AUTH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, ClientExceptionCode.AUTH_TOKEN_EXPIRED, "만료된 토큰입니다."),
+    AUTH_TOKEN_INVALID(HttpStatus.UNAUTHORIZED, ClientExceptionCode.AUTH_TOKEN_INVALID, "올바르지 않은 토큰 정보입니다."),
+    AUTH_TOKEN_MISMATCH(HttpStatus.UNAUTHORIZED, ClientExceptionCode.AUTH_TOKEN_MISMATCH, "액세스 토큰과 리프레시 토큰의 소유자가 일치하지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, ClientExceptionCode.USER_NOT_FOUND, "찾을 수 없는 유저입니다."),
+    ;
+    private final HttpStatus httpStatus;
+    private final ClientExceptionCode clientExceptionCode;
+    private final String message;
+
+    ExceptionCode(HttpStatus httpStatus, ClientExceptionCode clientExceptionCode, String message) {
+        this.httpStatus = httpStatus;
+        this.clientExceptionCode = clientExceptionCode;
+        this.message = message;
+    }
+}
