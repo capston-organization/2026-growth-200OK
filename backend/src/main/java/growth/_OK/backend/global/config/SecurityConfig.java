da5feae16ff61d51f70b29fee2914162d360ece1
9rin <flyrin1208@gmail.com>
2026-01-28 21:01:34 +0900

    feat: login&logout&refresh

--- backend/src/main/java/growth/_OK/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/growth/_OK/backend/global/config/SecurityConfig.java
@@ -1,31 +1,53 @@
 package growth._OK.backend.global.config;
 
+
+import growth._OK.backend.auth.jwt.JwtAuthenticationFilter;
+import growth._OK.backend.auth.jwt.JwtTokenProvider;
+import growth._OK.backend.auth.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService userDetailsService;
+    private final TokenService tokenService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtTokenProvider, userDetailsService, tokenService);
+    }
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/**").permitAll() // 추후 변경 예정
-                                .anyRequest().authenticated()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**",  "/error").permitAll() // 추후 변경 예정
+                        .anyRequest().authenticated()
                 )
-                //.addFilterBefore()
-                .csrf(AbstractHttpConfigurer::disable);
+                .addFilterBefore(
+                        jwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class
+                )
+                .cors(Customizer.withDefaults());
         return http.build();
     }
 }
