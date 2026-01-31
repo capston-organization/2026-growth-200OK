03aeb0315b008e5f830b279b6cc1315f169833e1
9rin <flyrin1208@gmail.com>
2026-01-31 20:48:10 +0900

    test: game 생성, 조회, 좋아요 테스트 코드

--- backend/src/test/java/growth/_OK/backend/BackendApplicationTests.java
+++ backend/src/test/java/growth/_OK/backend/BackendApplicationTests.java
@@ -1,9 +1,14 @@
 package growth._OK.backend;
 
+import growth._OK.backend.config.TestRedisConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = {TestRedisConfig.class})
 class BackendApplicationTests {
 
 	@Test
