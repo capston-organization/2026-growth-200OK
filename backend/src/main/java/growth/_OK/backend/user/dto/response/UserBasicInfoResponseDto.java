025a5146d6acf74b2530fc49a260ad2f96ca6b9b
9rin <flyrin1208@gmail.com>
2026-02-20 11:43:48 +0900

    feat: 로그인 온보딩 화면

---
+++ backend/src/main/java/growth/_OK/backend/user/dto/response/UserBasicInfoResponseDto.java
@@ -0,0 +1,41 @@
+package growth._OK.backend.user.dto.response;
+
+import growth._OK.backend.user.domain.Gender;
+import growth._OK.backend.user.domain.Role;
+import growth._OK.backend.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserBasicInfoResponseDto {
+    private final Long userId;
+    private final String name;
+    private final String nickname;
+    private final String profileImage;
+    private final Integer birthYear;
+    private final String grade;
+    private final LocalDate birthDate;
+    private final String school;
+    private final Gender gender;
+    private final Role role;
+
+    public static UserBasicInfoResponseDto from(User user) {
+        return UserBasicInfoResponseDto.builder()
+                .userId(user.getUserId())
+                .name(user.getName())
+                .nickname(user.getNickname())
+                .profileImage(user.getProfileImage())
+                .birthYear(user.getBirthYear())
+                .grade(user.getGrade())
+                .birthDate(user.getBirthDate())
+                .school(user.getSchool())
+                .gender(user.getGender())
+                .role(user.getRole())
+                .build();
+    }
+}
