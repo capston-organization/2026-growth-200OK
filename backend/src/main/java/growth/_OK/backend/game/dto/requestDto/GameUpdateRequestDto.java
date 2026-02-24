a7a6ead21b51846cd4bf3c848152558e235e7dd8
9rin <flyrin1208@gmail.com>
2026-02-24 18:04:50 +0900

    feat: 게임 제목, 설명, 공개방식 수정 api

---
+++ backend/src/main/java/growth/_OK/backend/game/dto/requestDto/GameUpdateRequestDto.java
@@ -0,0 +1,16 @@
+package growth._OK.backend.game.dto.requestDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameUpdateRequestDto {
+    private String title;
+    private String description;
+    private Boolean isPublic;
+}
